fca2d66fc273d4b027bfe69654f71c1a26c6d26d
DJshelfmushroom <calvin@kmikeym.com>
2026-09-17 13:54:18 -0700

    buffer output and get ready for nether generation

--- main.c
+++ main.c
@@ -7,13 +7,15 @@
 
 
 int main(int argc, char **argv) {
+    setvbuf(stdout, NULL, _IOFBF, 0);
     if (argc < 3) {
         fprintf(stderr, "usage: %s <seed|-1> <struct> [struct,struct,...]\n", argv[0]);
         return 1;
     }
     initConfigs(MC);
-    Generator g;
-    setupGenerator(&g, MC, 0);
+    Generator go, gn;
+    setupGenerator(&go, MC, 0);
+    setupGenerator(&gn, MC, 0);
     int64_t seed;
     int nwant = argc - 2;
     int want[nwant];
@@ -33,20 +35,24 @@ int main(int argc, char **argv) {
         regionSizes[i] = sc.regionSize << 4;
     }
     for (;;seed++) {
-        applySeed(&g, DIM_OVERWORLD, seed);
-        Pos spawn = getSpawn(&g);
+        applySeed(&go, DIM_OVERWORLD, seed);
+        applySeed(&gn, DIM_NETHER, seed);
+        Pos spawn = getSpawn(&go);
         int r0x, r1x, r0z, r1z;
         int px = spawn.x, pz = spawn.z;
         int minX = px - 96, maxX = px + 96;
         int minZ = pz - 96, maxZ = pz + 96;
         for (int i = 0; i < nwant; i++) {
             calcRegionBounds(regionSizes[i], minX, maxX, minZ, maxZ, &r0x, &r1x, &r0z, &r1z);
-            struct checkParams params = {want[i], &g, seed, spawn, r0x, r1x, r0z, r1z};
+            struct checkParams params = {want[i], &go, seed, spawn, r0x, r1x, r0z, r1z};
             Pos structPos;
             if (structureCheck(params, &structPos)) {
                 printf("HIT %" PRIi64 " %s %i,%i\n" , seed, struct2str(want[i]), structPos.x, structPos.z);
-                fflush(stdout);
+                if (ferror(stdout)) return 1;
             }
+        }
+        if (seed % 1000 == 0) {
+            fflush(stdout);
         }
     }
     return 0;
--- seedsearch.c
+++ seedsearch.c
@@ -43,53 +43,10 @@ bool structureCheck(struct checkParams params, Pos *outPos) {
     return false;
 }
 
+
 void calcRegionBounds(int regionSize, int minX, int maxX, int minZ, int maxZ, int *r0x, int *r1x, int *r0z, int *r1z) {
     *r0x = floordiv(minX,regionSize);
     *r1x = floordiv(maxX,regionSize);
     *r0z = floordiv(minZ,regionSize);
     *r1z = floordiv(maxZ,regionSize);
 }
-
-/*
-int main(int argc, char **argv) {
-    if (argc < 3) {
-        fprintf(stderr, "usage: %s <seed|-1> <struct> [struct,struct,...]\n", argv[0]);
-        return 1;
-    }
-    initConfigs(MC);
-    Generator g;
-    setupGenerator(&g, MC, 0);
-    int64_t seed;
-    int want[argc - 2];
-    // parse args, in order: [prog name, starting seed (-1 is random), feature numbers sequentially]
-    for (int i = 1; i < argc; i++) {
-        if (i == 1) {
-            uint64_t rng;
-            setSeed(&rng, time(NULL));
-            seed = strcmp(argv[1], "-1") == 0 ? (int64_t)nextLong(&rng) : strtoll(argv[1], NULL, 10);
-            continue;
-        }
-        want[i-2] = (int)strtol(argv[i], NULL, 10);
-    }
-    for (;;seed++) {
-        applySeed(&g, DIM_OVERWORLD, seed);
-        Pos spawn = getSpawn(&g);
-        int r0x, r1x, r0z, r1z;
-        int px = spawn.x, pz = spawn.z;
-        int minX = px - 96, maxX = px + 96;
-        int minZ = pz - 96, maxZ = pz + 96;
-        // Once per structure, get the region size (in blocks)
-        for (int i = 0; i < sizeof(want)/sizeof(want[0]); i++) {
-            int regionSize = configs[want[i]].regionSize << 4;
-            calcRegionBounds(regionSize, minX, maxX, minZ, maxZ, &r0x, &r1x, &r0z, &r1z);
-            struct checkParams params = {want[i], &g, seed, spawn, r0x, r1x, r0z, r1z};
-            Pos structPos;
-            if (structureCheck(params, &structPos)) {
-                printf("HIT %" PRIi64 " %s %i,%i\n" , seed, struct2str(want[i]), structPos.x, structPos.z);
-                fflush(stdout);
-            }
-        }
-    }
-    return 0;
-}
-*/
